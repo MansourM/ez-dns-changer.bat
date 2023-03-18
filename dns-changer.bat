f0ac512c9fdb3ba2125f7c7a39d92759305dbbe9
MansourM <sm.mirbehbahani@gmail.com>
2023-03-18 23:14:03 +0330

    :new: added the ability read server info from file

--- dns-changer.bat
+++ dns-changer.bat
@@ -25,6 +25,23 @@ goto :eof
      "Not NetConnectionStatus Is Null And NetEnabled='TRUE'" ^
      Get Name^,NetConnectionID /Value 2^> NUL') Do Set "%%G" 2> NUL 1>&2
     if Not Defined Name goto :eof
+    echo Detected network name is: %NetConnectionID%
+    ::netsh interface ip set dns name="%NetConnectionID%" static 1.1.1.1
+    ::netsh interface ip add dns name="%NetConnectionID%" 4.2.2.4 index=2
+    goto :readfile
+
+:readfile
+    set "x = 0"
+    for /f "tokens=1-3 delims=," %%i in (%2dns-servers.txt) do (
+     set Names[%x%]=%%i
+     ::call echo name=%%Names[%x%]%%
+     set Server1[%x%]=%%j
+     ::call echo Server1=%%Server1[%x%]%%
+     set Server2[%x%]=%%k
+     ::call echo Server2=%%Server2[%x%]%%
+     set /a "x+=1"
+    )
+
 
 pause
 goto :eof
